cdc772bec42a328230dd4cfa9f7692275ef08b13
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-07-08 22:52:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
+
+app.use(express.static('client'));
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('disconnect', () => console.log('User disconnected'));
+});
+
+server.listen(3000, () => console.log('Listening to the port 3000'));
---
+++ client/index.js
@@ -0,0 +1,3 @@
+const socket = io();
+
+const name = prompt('Enter your name to join chat');
